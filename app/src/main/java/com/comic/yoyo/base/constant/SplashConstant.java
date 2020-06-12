8a2a1d7ee9655b9edc6f029805e1a922990ee72c
zhixiaoxun <zhixiaoxun@163.com>
2020-06-12 22:09:40 +0800

    [feature]  完善开屏逻辑；添加隐私协议；tab icon替换；调整部分结构
    [what]  开屏添加动画；添加隐私协议；tab icon替换；添加layout和drawable的分类
    [how]  引入lottie，添加隐私协议的展示，layout按内容分类，app gradle中添加sourceSets配置
    [why] 新功能开发

---
+++ app/src/main/java/com/comic/yoyo/base/constant/SplashConstant.java
@@ -0,0 +1,14 @@
+package com.comic.yoyo.base.constant;
+
+public class SplashConstant {
+
+    public interface Policy{
+        public static final String PRIVACY_URL = "file:///android_asset/privacy.html";
+        public static final String SERVICE_URL ="file:///android_asset/service.html";
+
+        //隐私协议摘要
+        public static final String PRIVACY_POLICY_SUMMARY = "privacy_policya_summary";
+        public static final String PRIVACY_POLICY_SUMMARY_SHOWED = "privacy_policya_summary_showed";
+    }
+
+}
