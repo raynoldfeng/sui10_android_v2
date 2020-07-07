f01c46bcd9ce8e569f281a4324def45b3da5a929
zhixiaoxun <zhixiaoxun@163.com>
2020-07-07 22:01:54 +0800

    [feature]  添加专业课介绍页面的数据
    [what]  添加专业课介绍页面的数据
    [how]  有数据时展示课程介绍里的图片列表；没有时展示空页面
    [why] 新功能开发

---
+++ app/src/main/java/com/sui10/suishi/module/course/adapter/CourseImgIntroAdapter.java
@@ -0,0 +1,46 @@
+package com.sui10.suishi.module.course.adapter;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sui10.suishi.R;
+import com.sui10.suishi.common.ui.adapter.BaseAdapter;
+import com.sui10.suishi.common.utils.ImageLoadUtils;
+
+public class CourseImgIntroAdapter extends BaseAdapter<String,CourseImgIntroAdapter.CourseImgIntroViewHolder> {
+
+
+    @Override
+    public CourseImgIntroViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_course_img_intro, viewGroup,false);
+        return new CourseImgIntroViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(CourseImgIntroViewHolder holder, int i) {
+        String url = mDataList.get(i);
+        if(!TextUtils.isEmpty(url))
+            ImageLoadUtils.loadImage(url,holder.mIntroIv);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+    static class CourseImgIntroViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView mIntroIv;
+
+        public CourseImgIntroViewHolder(View view){
+            super(view);
+            mIntroIv = view.findViewById(R.id.course_intro_iv);
+        }
+    }
+}
--- app/src/main/java/com/sui10/suishi/module/course/ui/CourseIntroFragment.java
+++ app/src/main/java/com/sui10/suishi/module/course/ui/CourseIntroFragment.java
@@ -1,16 +1,28 @@
 package com.sui10.suishi.module.course.ui;
 
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sui10.commonlib.ui.presenter.BasePresenter;
 import com.sui10.commonlib.ui.view.base.BaseFragment;
+import com.sui10.commonlib.utils.DensityUtils;
+import com.sui10.commonlib.utils.ResourceUtils;
 import com.sui10.suishi.R;
+import com.sui10.suishi.module.course.adapter.CourseImgIntroAdapter;
+import com.sui10.suishi.module.course.bean.CourseBean;
+
+import java.util.List;
 
 import butterknife.BindView;
 
 public class CourseIntroFragment extends BaseFragment {
     private LinearLayoutManager mLayoutManager;
+    private CourseImgIntroAdapter mAdapter;
+    private CourseBean mCourseBean;
 
     @BindView(R.id.course_intro_data_rv)
     RecyclerView mCourseIntroRv;
@@ -34,7 +46,32 @@ public class CourseIntroFragment extends BaseFragment {
     public void initViewsAndEvents() {
         mLayoutManager=new LinearLayoutManager(getContext());
         mCourseIntroRv.setLayoutManager(mLayoutManager);
+        mCourseIntroRv.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                outRect.top = DensityUtils.dip2px(getActivity(),16);
+            }
+        });
+
+        mAdapter = new CourseImgIntroAdapter();
+        mCourseIntroRv.setAdapter(mAdapter);
+        initData();
     }
+
+    private void initData(){
+        Bundle bundle = getArguments();
+        if(bundle != null){
+            mCourseBean = (CourseBean)bundle.getSerializable(ProfessionalCourseDetailActivity.COURSE_BEAN_KEY);
+            List<String> imgIntros = mCourseBean.getImgIntro();
+            if(imgIntros != null && !imgIntros.isEmpty()){
+                mAdapter.setData(imgIntros);
+            }else {
+                showNoData(ResourceUtils.getString(R.string.prepareing));
+            }
+        }
+    }
+
 
     @Override
     protected void onNetworkConnected(int type) {
