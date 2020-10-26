92503dd9169db1f66fe1aa92b20a4ff3e2c10636
zhixiaoxun <zhixiaoxun@163.com>
2020-10-26 20:23:26 +0800

    [feature]  添加个人中心页面UI及部分跳转逻辑
    [what]  添加个人中心页面UI及部分跳转逻辑
    [how]  未登录时展示未登录提示，未登录时点击跳转至登录页
    [why] 新功能开发

--- app/src/main/java/com/sui10/suishi/module/usersystem/ui/MeFragment.java
+++ app/src/main/java/com/sui10/suishi/module/usersystem/ui/MeFragment.java
@@ -1,10 +1,39 @@
 package com.sui10.suishi.module.usersystem.ui;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.sui10.commonlib.ui.presenter.BasePresenter;
+import com.sui10.commonlib.ui.utils.ToastUtils;
 import com.sui10.commonlib.ui.view.base.BaseFragment;
 import com.sui10.suishi.R;
+import com.sui10.suishi.common.ui.JumpManager;
+import com.sui10.suishi.manager.UserManager;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MeFragment extends BaseFragment {
+
+    @BindView(R.id.menu_profile_desc_tv)
+    TextView mDescTv;
+    @BindView(R.id.menu_profile_nick_tv)
+    TextView mNickTv;
+    @BindView(R.id.menu_profile_avatar_iv)
+    ImageView mAvatarTv;
+    @BindView(R.id.not_login_tips_tv)
+    TextView mNotLoginTipsTv;
+
+
+    @BindView(R.id.menu_item_user_info)
+    RelativeLayout mUserInfoRl;
+    @BindView(R.id.menu_item_my_course)
+    RelativeLayout mMyCourseRl;
+    @BindView(R.id.menu_item_setting)
+    RelativeLayout mSettingRl;
+
 
     @Override
     protected int getContentViewLayoutID() {
@@ -24,6 +53,11 @@ public class MeFragment extends BaseFragment {
     @Override
     public void initViewsAndEvents() {
         setTitle(R.string.tab_mine_text);
+        if(!UserManager.getInstance().isLogin()){
+            mNotLoginTipsTv.setVisibility(View.VISIBLE);
+        }else {
+            mNotLoginTipsTv.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -35,4 +69,36 @@ public class MeFragment extends BaseFragment {
     protected void onNetworkDisConnected() {
 
     }
+
+    @OnClick(R.id.login_btn)
+    public void onLoginBtnClicked() {
+        JumpManager.gotoLoginActivity(getActivity());
+    }
+
+    @OnClick(R.id.menu_item_user_info)
+    public void onMenuUserInfoClicked() {
+        checkLoginState();
+
+    }
+
+    @OnClick(R.id.menu_item_my_course)
+    public void onMenuMyCourseClicked() {
+        checkLoginState();
+    }
+
+    @OnClick(R.id.menu_item_setting)
+    public void onMenuSettingClicked() {
+
+    }
+
+    private boolean checkLoginState(){
+        if(!UserManager.getInstance().isLogin()) {
+            ToastUtils.showShort(R.string.please_login_first);
+            JumpManager.gotoLoginActivity(getActivity());
+            return false;
+        }
+        return true;
+    }
+
+
 }
